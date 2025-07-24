dd04fe1dd63f8d322dd2fd9c771d1052f806a9c1
Josh Wulf <josh.wulf@camunda.com>
2025-07-24 19:37:22 +1200

    style: fix linting of jest.config.js
    Signed-off-by: Josh Wulf <josh.wulf@camunda.com>

--- prettier.config.js
+++ prettier.config.js
@@ -10,6 +10,14 @@ module.exports = {
 				useTabs: false,
 			},
 		},
+		{
+			files: ['*.config.js', 'jest.config.js'],
+			options: {
+				parser: 'babel',
+				useTabs: true,
+				tabWidth: 2,
+			},
+		},
 	],
 	semi: false,
 	singleQuote: true,
